dcc5d88740bf15477b7374419b322e0caf68e688
Gaston Jaurena <132623094+gastonjau@users.noreply.github.com>
2023-08-25 20:08:34 -0300

    Repo de Lista Tareas
    Grupo 1

---
+++ js/almacenar.js
@@ -0,0 +1,26 @@
+//item, agregar
+document.addEventListener("DOMContentLoaded",()=>{
+    let inicio = localStorage.getItem("agregoItem");
+    var contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML = `<h2>${inicio}</h2>`;
+    const agregar = document.getElementById("agregar");
+    agregar.addEventListener("click", ()=> {
+        
+        const item = document.getElementById("item").value;
+        localStorage.setItem("agregoItem", item)
+        let crearI = localStorage.getItem("agregoItem");
+                var clave = `clave`;
+                const valor = `valor`;
+                localStorage.setItem(clave, valor);
+            let li = document.createElement("li");
+            li.innerHTML = `<h2>${item}</h2>`
+            contenedor.appendChild(li);
+    })
+    const limpiar = document.getElementById("limpiar");
+
+        limpiar.addEventListener("click", ()=>{
+        localStorage.removeItem("agregoItem");
+        contenedor.textContent = " ";
+    
+    })
+})
